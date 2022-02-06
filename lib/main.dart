b03d9e9ca6fbc36e591a7d2a1edf71dfef2ea848
MB-Linux <mb-linux>
2022-02-06 19:58:29 +0400

    New comment here

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class playground extends StatefulWidget {
+  @override
+  _playgroundState createState() => _playgroundState();
+}
+
+class _playgroundState extends State<playground> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+//!comment
+//todo create an app
+//normal comment
